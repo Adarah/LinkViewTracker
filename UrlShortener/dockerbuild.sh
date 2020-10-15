7788245e5514669096180f4601ad77f22843bd39
Adarah <lucasyharada@gmail.com>
2020-10-15 18:22:59 -0300

    Added containerization recipe

---
+++ UrlShortener/dockerbuild.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+flyway migrate -url=jdbc:postgresql://localhost:5432/url_shortener -user=${DB_USER} -password=${DB_PASSWORD} -locations="classpath:db/migration/"
+mvn generate-sources
+mvn package
+docker build . -t adarah/url_shortener:0.0.3
